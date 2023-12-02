b302e748582be1f7411da879702d96a334d9459c
JeevikaSuresh <145818325+JeevikaSuresh@users.noreply.github.com>
2023-12-02 11:49:34 -0500

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -28,6 +28,29 @@ def Image_Match():
     import scipy as sc
 
     st.title("Multi Modal Retrieval Model ")
+   import streamlit as st
+    import cv2
+    import ntpath
+    import matplotlib.pyplot as plt
+    import glob
+    from serpapi import GoogleSearch
+
+    # Replace this with the full path to your directory
+    directory_path = 'Data'
+
+    # Use the directory_path in the glob function
+    image_paths = glob.glob(f'{directory_path}/*.jpg')
+
+    # Rest of the code remains the same
+    #st.write(f'Found [{len(image_paths)}] images')
+
+    images = {}
+    for image_path in image_paths:
+        image = cv2.imread(image_path, 3)
+        b, g, r = cv2.split(image)           # get b, g, r
+        image = cv2.merge([r, g, b])         # switch it to r, g, b
+        image = cv2.resize(image, (200, 200))
+        images[ntpath.basename(image_path)] = image
 
 
     
